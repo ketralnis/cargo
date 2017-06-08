cc635b25018803ff6c53018d3798946425c6fa7f
Travis CI User <travis@example.org>
2017-06-08 18:22:25 +0000

    Update documentation

---
+++ implementors/rustc_serialize/serialize/trait.Decodable.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["cargo"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/rustc-serialize/rustc_serialize/serialize/trait.Decodable.html\" title=\"trait rustc_serialize::serialize::Decodable\">Decodable</a> for <a class=\"enum\" href=\"cargo/ops/enum.MessageFormat.html\" title=\"enum cargo::ops::MessageFormat\">MessageFormat</a>","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/rustc-serialize/rustc_serialize/serialize/trait.Decodable.html\" title=\"trait rustc_serialize::serialize::Decodable\">Decodable</a> for <a class=\"enum\" href=\"cargo/ops/enum.VersionControl.html\" title=\"enum cargo::ops::VersionControl\">VersionControl</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ implementors/rustc_serialize/serialize/trait.Encodable.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["cargo"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/rustc-serialize/rustc_serialize/serialize/trait.Encodable.html\" title=\"trait rustc_serialize::serialize::Encodable\">Encodable</a> for <a class=\"enum\" href=\"cargo/util/config/enum.ConfigValue.html\" title=\"enum cargo::util::config::ConfigValue\">ConfigValue</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
